817b24753b2ed11d2ddbbbad5c03f0d07c6142b7
Rishi Kosta <rishikosta58@gmail.com>
2026-01-11 23:23:12 +0530

    Hello-Bridgelabz

---
+++ Basic-Programmes/SumOff2No.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumOff2No {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 2 no: ");
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        System.out.print("the sum of 2 no is: "+sum);
+
+
+
+    }
+
+}
