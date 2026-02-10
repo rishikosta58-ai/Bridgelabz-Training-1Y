4ac9195f7040d64804a4773e327c437e599b84cf
Rishi Kosta <rishikosta58@gmail.com>
2026-02-10 23:01:55 +0530

    [Rishi] IllegalArgDemo

---
+++ src/com/gla/string/level1/IllegalArgDemo.java
@@ -0,0 +1,24 @@
+package com.gla.string.level1;
+
+import java.util.Scanner;
+public class IllegalArgDemo{
+    public static void generate(String s){
+        System.out.println(s.substring(5,2));
+    }
+    public static void handle(String s){
+        try{
+            System.out.println(s.substring(5,2));
+        }catch(IllegalArgumentException e){
+            System.out.println("Handled");
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        try{
+            generate(s);
+        }catch(Exception e){
+        }
+        handle(s);
+    }
+}
