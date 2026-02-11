d40dc0f2a5b5ddcaa316d5fa0b87dffc57b68fab
Rishi Kosta <rishikosta58@gmail.com>
2026-02-11 22:17:12 +0530

    [Rishi] PalindromeCheck2

---
+++ src/com/gla/string/problem_statement/PalindromeCheck2.java
@@ -0,0 +1,21 @@
+package com.gla.string.problem_statement;
+
+import java.util.Scanner;
+public class PalindromeCheck2{
+    public static boolean check(String s){
+        int st=0,en=s.length()-1;
+        while(st<en){
+            if(s.charAt(st)!=s.charAt(en)){
+                return false;
+            }
+            st++;
+            en--;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(check(s));
+    }
+}
