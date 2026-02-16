a93b254b493674e24ec3a7974c695e3af385353a
Rishi Kosta <rishikosta58@gmail.com>
2026-02-16 22:14:00 +0530

    Assignment6

---
+++ src/com/gla/class_objects/level1/Handshakes.java
@@ -0,0 +1,13 @@
+package com.gla.class_objects.level1;
+
+import java.util.Scanner;
+public class Handshakes{
+    public static int count(int n){
+        return n*(n-1)/2;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(count(n));
+    }
+}
