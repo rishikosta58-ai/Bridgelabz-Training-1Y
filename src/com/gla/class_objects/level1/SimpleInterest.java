2163bf4bb12ae2d4afc146a508c76375d575b577
Rishi Kosta <rishikosta58@gmail.com>
2026-02-16 22:53:32 +0530

    Assignment6

---
+++ src/com/gla/class_objects/level1/SimpleInterest.java
@@ -0,0 +1,16 @@
+package com.gla.class_objects.level1;
+
+import java.util.Scanner;
+public class SimpleInterest{
+    public static double calculate(double p,double r,double t){
+        return p*r*t/100;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double p=sc.nextDouble();
+        double r=sc.nextDouble();
+        double t=sc.nextDouble();
+        double si=calculate(p,r,t);
+        System.out.println("The Simple Interest is "+si+" for Principal "+p+", Rate "+r+" and Time "+t);
+    }
+}
