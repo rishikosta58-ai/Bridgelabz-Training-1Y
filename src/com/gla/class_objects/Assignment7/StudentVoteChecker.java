5990733c2fa0c3b303332b647ce63294d379d63d
Rishi Kosta <rishikosta58@gmail.com>
2026-02-16 23:30:15 +0530

    Assignment6

---
+++ src/com/gla/class_objects/Assignment7/StudentVoteChecker.java
@@ -0,0 +1,21 @@
+package com.gla.class_objects.Assignment7;
+
+import java.util.Scanner;
+public class StudentVoteChecker{
+    public static boolean canVote(int age){
+        if(age<0){
+            return false;
+        }
+        if(age>=18){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<10;i++){
+            int age=sc.nextInt();
+            System.out.println(canVote(age));
+        }
+    }
+}
